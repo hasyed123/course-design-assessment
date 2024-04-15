408d53fd6e4204b560b28ae6fd4425eb7c83cd7c
hasyed <hasansyed12s@outlook.com>
2024-04-15 07:38:03 -0400

    added delete and get all courses to driver

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         enroll_succeeded = course_service.enroll_student(course_id, student_id)
         print(f"Enrolled student {student_id}: {enroll_succeeded}")
 
-    # Create assignments and store their IDs
+    # Create assignments
     assignment_ids = {}
     for assignment_id in range(1, 4):
         assignment_name = f"Assignment {assignment_id}"
@@ -22,10 +22,9 @@ if __name__ == "__main__":
         assignment_ids.update({new_assignment_id: assignment_name})
         print(f"Created assignment: {assignment_name} with ID: {new_assignment_id}")
 
-    # Submit assignments using the stored assignment IDs with random grades between 0 and 100
+    # Submit assignments with random grades between 0 and 100
     for student_id in range(1, 6):
         for assignment_id in assignment_ids:
-            # Generate a random grade between 0 and 100
             grade = random.randint(0, 100)
             submit_succeeded = course_service.submit_assignment(course_id, student_id, assignment_id, grade)
             print(f"Submitted assignment {assignment_ids.get(assignment_id)} for student {student_id} with grade {grade}: {submit_succeeded}")
@@ -43,3 +42,22 @@ if __name__ == "__main__":
     # Display top five students
     top_students = course_service.get_top_five_students(course_id)
     print(f"Top five students in course {course_id}: {top_students}")
+
+    # Create another course
+    course_name_2 = "Course 2"
+    course_id_2 = course_service.create_course(course_name)
+    print(f"Created course: {course_name_2} with ID: {course_id_2}")
+
+    # Display all courses
+    all_courses = course_service.get_courses()
+    print(f"All courses: {list(map(lambda course: course.id, all_courses))}")
+
+    # Delete both courses
+    delete_succeeded = course_service.delete_course(course_id)
+    print(f"Deleted course id {course_id}: {delete_succeeded}")
+    delete_succeeded = course_service.delete_course(course_id_2)
+    print(f"Deleted course id {course_id_2}: {delete_succeeded}")
+
+    # Display all courses
+    all_courses = course_service.get_courses()
+    print(f"All courses: {list(map(lambda course: course.id, all_courses))}")
